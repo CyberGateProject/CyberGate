f8240ac98fb79d8ecafa26aa659ee78be2b5b8b9
HappyHippyHippo <carlosjsldias@gmail.com>
2016-05-28 19:29:49 +0100

    streams and threads

--- Skeleton/source/main.cpp
+++ Skeleton/source/main.cpp
@@ -1,36 +1,15 @@
-#include "cgate/log/logger_factory.hpp"
-#include "SFML/Graphics.hpp"
+#include "cgate/cgate.hpp"
 
-#if defined(_WIN32)
-#include <Windows.h>
+#include "SDL2/SDL_platform.h"
 
-int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-#else
+#include <stdio.h>
+
 int main(int argc, char** argv)
-#endif
 {
-    cgate::LoggerFactory::getSingleton().createFileLogger("file1", "file1.txt", true);
-    cgate::LoggerFactory::getSingleton().createFileLogger("file2", "file2.txt", true);
+    cgate::LoggerFactory::createConsoleLogger("file1");
+    cgate::LoggerFactory::createConsoleLogger("file2");
+    cgate::LoggerFactory::fatal(0, "FATAL ERROR : %s", "I am to good for this shit !!!");
 
-    cgate::LoggerFactory::getSingleton().fatal(0, "FATAL ERROR : %s\n", "I am to good for this shit !!!");
-/*
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-*/
+    getchar();
     return 0;
 }
